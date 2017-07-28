ff17a8f666e7f45d1d2a3e76725347b9ae6641b2
Bobby <karma0@gmail.com>
2017-07-28 12:47:06 -0600

    Changed colors

--- .i3/pystatus.py
+++ .i3/pystatus.py
@@ -17,9 +17,13 @@ status = Status(standalone=True)
 # Tue 30 Jul 11:59:46 PM W31
 #                          ^-- calendar week
 status.register("clock",
+  color="#E6E1CF",
   format="%a %-d %b %X W%V",)
 
 status.register("mem_bar",
+  color="#B8CC52",
+  alert_color="#FF3333",
+  multi_colors=True,
   format="^{used_mem_bar}")
 
 # The battery monitor has many formatting options, see README for details
@@ -37,6 +41,11 @@ status.register("battery",
   format="{status}/{percentage:.2f}% {remaining:%E%hh:%Mm}",
   alert=True,
   alert_percentage=5,
+  color="#FFEE99",
+  full_color="#B8CC52",
+  charging_color="#E6E1CF",
+  critical_color="#ff3333",
+  not_present_color="#FFEE99",
   status={
     "DIS": "↓",
     "CHR": "↑",
@@ -57,7 +66,10 @@ status.register("battery",
 
 # Shows the average load of the last minute and the last 5 minutes
 # (the default value for format is used)
-status.register("load")
+status.register("load",
+  color="#B8CC52",
+  critical_color="#FF3333",
+)
 
 # Shows your CPU temperature, if you have a Intel CPU
 #status.register("temp",
@@ -73,6 +85,8 @@ for c in range(cpu_count()):
 
 fmt_str = fmt_str[:-1]
 status.register("cpu_usage_graph",
+  start_color="#B8CC52",
+  end_color="#FF3333",
   format=fmt_str,
   #format_all="{core}:{usage:02}%",
 )
@@ -81,6 +95,8 @@ status.register("cpu_usage_graph",
 # Format:
 # 42/128G [86G]
 status.register("disk",
+  critical_color="#FF3333",
+  color="#B8CC52",
   path="/",
   format="{used}/{total}G [{avail}G]",)
 
@@ -94,6 +110,8 @@ status.register("disk",
 
 # Displays whether a DHCP client is running
 status.register("runwatch",
+  color_up="#B8CC52",
+  color_down="#FF3333",
   name="DHCP",
   path="/run/dhcpcd-*.pid",)
 
