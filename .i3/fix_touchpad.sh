3c8e9dc17b4f38342bd14927872884042d2c775f
Bobby <karmma0@gmail.com>
2018-09-08 13:41:44 -0600

    Added inverted scrolling

--- .i3/fix_touchpad.sh
+++ .i3/fix_touchpad.sh
@@ -2,7 +2,9 @@
 
 ID=$(xinput list | grep Synaptics\ TouchPad | cut -d= -f2 | awk '{print $1}')
 TAP_FEATURE=$(xinput list-props $ID | grep "Tapping Enabled (" | cut -d'(' -f2 | cut -d')' -f1)
+SCROLL_FEATURE=$(xinput list-props $ID | grep "Natural Scrolling Enabled (" | cut -d'(' -f2 | cut -d')' -f1)
 
 xinput set-prop $ID $TAP_FEATURE 1
+xinput set-prop $ID $SCROLL_FEATURE 0
 
 exit 0
